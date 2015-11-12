b24de8ed88161d64c9f9a7c229557a27ef52f241
Walter Moreira <walter@waltermoreira.net>
2015-11-12 12:51:55 -0600

    Add oauth authentication [WIP]

--- roles/jupyterhub/files/jupyterhub/jupyterhub_config.py
+++ roles/jupyterhub/files/jupyterhub/jupyterhub_config.py
@@ -1,3 +1,6 @@
+import os
+
+
 c = get_config()
 c.JupyterHub.spawner_class = 'dockerspawner.SystemUserSpawner'
 
@@ -6,3 +9,14 @@ c.JupyterHub.hub_ip = jupyter_client.localinterfaces.public_ips()[0]
 
 c.DockerSpawner.container_ip = '172.17.42.1'
 c.SystemUserSpawner.host_homedir_format_string = '/home/{username}'
+
+c.JupyterHub.authenticator_class = 'oauthenticator.AgaveOAuthenticator'
+
+c.AgaveOAuthenticator.oauth_callback_url = os.environ['OAUTH_CALLBACK_URL']
+c.AgaveOAuthenticator.client_id = os.environ['AGAVE_CLIENT_ID']
+c.AgaveOAuthenticator.client_secret = os.environ['AGAVE_CLIENT_SECRET']
+c.AgaveOAuthenticator.agave_base_url = os.environ['AGAVE_BASE_URL']
+c.AgaveOAuthenticator.agave_base_url = os.environ['AGAVE_TENANT_NAME']
+
+c.AgaveMixin.agave_base_url = os.environ['AGAVE_BASE_URL']
+
